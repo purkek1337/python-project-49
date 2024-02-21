28fa7c416ec0414e739293fc4606a5a641e3cafe
purkek <aleksafomich@gmail.com>
2024-02-21 23:18:53 +0300

    add comments

--- brain_games/even_games.py
+++ brain_games/even_games.py
@@ -2,7 +2,7 @@ from random import randint
 from brain_games.cli import welcome_user
 
 
-def is_even(number):
+def is_even(number): # функция проверки чётности числа
     if number % 2 == 0:
         return 'yes'
     else:
@@ -10,17 +10,17 @@ def is_even(number):
 
 
 def even_game():
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    for i in range(3):
-        number = randint(1, 100)
-        print(f'Question: {number}')
-        user_answer = input('Your answer: ')
-        if is_even(number) == user_answer:
-            print('Correct!')
-        else:
+    name = welcome_user() # выполняем приветствие и возвращаем имя
+    print('Answer "yes" if the number is even, otherwise answer "no".') # правила игры
+    for i in range(3): # в игре 3 раунда
+        number = randint(1, 100) # создаем переменную рандомного числа
+        print(f'Question: {number}') # выводим вопрос
+        user_answer = input('Your answer: ') # вводим ответ
+        if is_even(number) == user_answer: # сравниваем вопрос с ответом
+            print('Correct!') # верный ответ
+        else: # неверный ответ
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was "
                   f"'{is_even(number)}'")
             print(f"Let's try again, {name}!")
-            return
-    print(f'Congratulations {name}!')
+            return # программа закрывается
+    print(f'Congratulations {name}!') # при трёх правильных ответах игрок побеждает
